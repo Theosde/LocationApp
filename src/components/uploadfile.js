c46bebc2368299ab264bf93d8639a45f717f7b70
Theo <theo.lassiaz@sfr.fr>
2019-11-15 14:42:34 +0100

    probleme conflit

--- src/components/uploadfile.js
+++ src/components/uploadfile.js
@@ -15,18 +15,6 @@ import url from '../config';
 function Upload() {
 
   const[dataFile,setDataFile] = useState({})
-<<<<<<< HEAD
-  const[userData,setUserData] = useState({})
-
-useEffect(()=>{
-  if ( JSON.parse(sessionStorage.getItem("user")) == null ) {
-    console.log("localStorage vide");
-  }else {
-    console.log("localStorage", JSON.parse(sessionStorage.getItem("user")) );
-    setUserData(JSON.parse(sessionStorage.getItem("user")))
-  }
-},[])
-=======
   const[valueInput,setValueInput] = useState("")
   const[userData,setUserData] = useState({})
 
@@ -39,7 +27,6 @@ useEffect(()=>{
       setUserData(JSON.parse(sessionStorage.getItem("user")))
     }
   },[])
->>>>>>> ede9ec0417e98d3d9606fdda6adb73d7338b2ac6
 
 
   var handleChangeFile = (event) => {
@@ -92,10 +79,6 @@ useEffect(()=>{
       <Header/>
       <div className="body-bg-doc">
 
-<<<<<<< HEAD
-
-=======
->>>>>>> ede9ec0417e98d3d9606fdda6adb73d7338b2ac6
         {userData.statususer=='proprio'?<div className="main-info-doc-3">
           <h1>Selectionner un destinataire</h1>
           <select type='select'>
@@ -105,28 +88,12 @@ useEffect(()=>{
             <option value="">Riad Salem</option>
             <option value="">Pierre Chatanay</option>
           </select>
-<<<<<<< HEAD
 
         </div>:<div></div>}
 
-
-=======
-        </div>:<div></div>}
-
->>>>>>> ede9ec0417e98d3d9606fdda6adb73d7338b2ac6
         <div className="main-info-doc-1">
 
           <h1> Envoyer un document </h1>
-<<<<<<< HEAD
-          {userData.statususer=='proprio'?<div style={{display:"flex",justifyContent:"center"}}>
-            <input className="checkbox-upload" type="radio" id="scales" name="scales"></input><label for="scales">Quittance</label>
-            <input className="checkbox-upload" type="radio" id="bail" name="scales"></input><label for="scales">Bail</label>
-            <input className="checkbox-upload" type="radio" id="courrier" name="scales"></input><label for="scales">Courrier</label>
-          </div> : <div style={{display:"flex",justifyContent:"center"}}>
-            <input className="checkbox-upload" type="radio" id="bail" name="scales"></input><label for="scales">Bail</label>
-            <input className="checkbox-upload" type="radio" id="courrier" name="scales"></input><label for="scales">Courrier</label>
-          </div>}
-=======
 
           {userData.statususer == "proprio" ? <div style={{display:"flex",justifyContent:"center"}}>
 
@@ -169,20 +136,13 @@ useEffect(()=>{
           </div>
           }
 
->>>>>>> ede9ec0417e98d3d9606fdda6adb73d7338b2ac6
           <div className="choose-file"><input className="choose-file-btn" type="file" name="file" onChange={handleChangeFile}/></div>
           <button className="upload-btn" type="button" onClick={handleClickConfirm}>Envoyer</button>
         </div>
 
-<<<<<<< HEAD
-        </div>
-=======
->>>>>>> ede9ec0417e98d3d9606fdda6adb73d7338b2ac6
-
 
         <div className="main-info-doc-2">
           <h1> Documents reçu </h1>
-<<<<<<< HEAD
           {userData.statususer=='locataire'?<div><div className="quittance menu">Quittances De Loyer</div>
           <div className="link-list">
             <a href="http://res.cloudinary.com/dyt3mhoy6/image/upload/v1571406071/eoddvew2fhiktk12cak2.pdf">Quittance Novembre 2019</a>
@@ -199,30 +159,6 @@ useEffect(()=>{
           </div>
         </div>
 
-        {userData.statususer=='proprio'?<div className="main-info-doc-2">
-          <h1> Documents envoyés </h1>
-          <div className="quittance menu">Quittances De Loyer</div>
-=======
-          {userData.statususer == "proprio" ? <div></div> : <div>
-            <div className="quittance menu">Quittances De Loyer</div>
-            <div className="link-list">
-              <a href="http://res.cloudinary.com/dyt3mhoy6/image/upload/v1571406071/eoddvew2fhiktk12cak2.pdf">Quittance Novembre 2019</a>
-              <a href="http://res.cloudinary.com/dyt3mhoy6/image/upload/v1571406071/eoddvew2fhiktk12cak2.pdf">Quittance Octobre 2019</a>
-              <a href="http://res.cloudinary.com/dyt3mhoy6/image/upload/v1571406071/eoddvew2fhiktk12cak2.pdf">Quittance Septembre 2019</a>
-            </div>
-          </div>
-
-          }
-          <div className="bail menu">Bails de location</div>
->>>>>>> ede9ec0417e98d3d9606fdda6adb73d7338b2ac6
-          <div className="link-list">
-            <a href="http://res.cloudinary.com/dyt3mhoy6/image/upload/v1571406071/eoddvew2fhiktk12cak2.pdf">Bail de location 2018 - 2019</a>
-          </div>
-          <div className="courriers-divers menu">Courriers</div><div className="link-list">
-            <a href="http://res.cloudinary.com/dyt3mhoy6/image/upload/v1571406071/eoddvew2fhiktk12cak2.pdf">Devis travaux</a>
-            <a href="http://res.cloudinary.com/dyt3mhoy6/image/upload/v1571406071/eoddvew2fhiktk12cak2.pdf">Suite : Rappel d'échéance</a>
-          </div>
-        </div>
 
         {userData.statususer=='proprio'?<div className="main-info-doc-2">
             <h1> Documents envoyés </h1>
@@ -250,22 +186,8 @@ useEffect(()=>{
             <a href="http://res.cloudinary.com/dyt3mhoy6/image/upload/v1571406071/eoddvew2fhiktk12cak2.pdf">Accord travaux</a>
             <a href="http://res.cloudinary.com/dyt3mhoy6/image/upload/v1571406071/eoddvew2fhiktk12cak2.pdf">Rappel d'échéance</a>
           </div>
-<<<<<<< HEAD
-        </div>
-        :<div className="main-info-doc-2"><h1> Documents envoyés </h1>
-        <div className="bail menu">Bails de location</div>
-        <div className="link-list">
-          <a href="http://res.cloudinary.com/dyt3mhoy6/image/upload/v1571406071/eoddvew2fhiktk12cak2.pdf">Bail de location 2018 - 2019</a>
-        </div>
-        <div className="courriers-divers menu">Courriers</div><div className="link-list">
-          <a href="http://res.cloudinary.com/dyt3mhoy6/image/upload/v1571406071/eoddvew2fhiktk12cak2.pdf">Accord travaux</a>
-          <a href="http://res.cloudinary.com/dyt3mhoy6/image/upload/v1571406071/eoddvew2fhiktk12cak2.pdf">Rappel d'échéance</a>
-        </div>
-      </div>}
-=======
-        </div>}
->>>>>>> ede9ec0417e98d3d9606fdda6adb73d7338b2ac6
 
+        </div>}
 
 
 
